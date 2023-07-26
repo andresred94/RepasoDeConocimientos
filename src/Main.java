2cffb9b6b26846664ded961ac5596e97c04bd79e
andres <bravocastro.andresmartin@gmail.com>
2023-07-26 20:09:46 +0200

    aaaa

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("%s\n%s\n","Bienvenido a","la programacion en Java!");
+
+        System.out.println("otro cambio");
+        System.out.println("otro canmasd");
+
+        //ASAS
+
+    }
+}
